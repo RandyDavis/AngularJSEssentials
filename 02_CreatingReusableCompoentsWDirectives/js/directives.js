2175c40f6c499c2dbed1993b3b36175acbe5fb58
Randy Davis <randy_davis@outlook.com>
2016-07-24 01:42:46 -0500

    working on Creating our own directives;  finish initial implementation but revising it each time to show how to eventually use it the best way;  Finished first part of scope showing @ sign use in a directive.

--- 02_CreatingReusableCompoentsWDirectives/js/directives.js
+++ 02_CreatingReusableCompoentsWDirectives/js/directives.js
@@ -1,3 +1,25 @@
 /**
  * Created by randy on 7/24/16.
  */
+// Directive via Template
+// parking.directive("alert", function () {
+//     return {
+//         template: "<div class='alert'>" +
+//             "<span class='alert-topic'>Something went wrong!</span>" +
+//             "<span class='alert-description'>You must inform the plate and the color of the car!</span>" +
+//             "</div>"
+//     };
+// });
+
+// Directive via TemplateURL
+parking.directive('alert', function () {
+    return {
+        restrict: 'E',
+        scope: {
+          topic: '@topic',
+          description:'@description'
+        },
+        templateUrl: "alert.html",
+        replace: true
+    };
+});
