5b9f379bcefb651c4f5868684d6368c85f139268
Randy Davis <randy_davis@outlook.com>
2016-07-24 02:07:25 -0500

    Finished part of creating our own directives scope part using "&" and add closeAlert functionality for the example

--- 02_CreatingReusableCompoentsWDirectives/js/controllers.js
+++ 02_CreatingReusableCompoentsWDirectives/js/controllers.js
@@ -3,8 +3,12 @@
  */
 parking.controller("parkingCtrl", ["$scope", function ($scope) {
     $scope.appTitle = "[Packt] Parking";
+    $scope.showAlert = true;
     $scope.alertTopic = "Something went wrong!";
     $scope.alertDescription = "You must inform the plate and the color of the car!";
+    $scope.closeAlert = function () {
+        $scope.showAlert = false;
+    }
     $scope.cars = [];
     $scope.colors = ["White", "Black", "Blue", "Red", "Silver"];
     $scope.park = function(car) {
--- 02_CreatingReusableCompoentsWDirectives/js/directives.js
+++ 02_CreatingReusableCompoentsWDirectives/js/directives.js
@@ -17,7 +17,8 @@ parking.directive('alert', function () {
         restrict: 'E',
         scope: {
           topic: '=topic',
-          description:'=description'
+          description:'=description',
+          close: '&close'
         },
         templateUrl: "alert.html",
         replace: true
