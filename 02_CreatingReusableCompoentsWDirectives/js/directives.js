6525b0f37d1c7b6f9817b33418bcc3266be58cb9
Randy Davis <randy_davis@outlook.com>
2016-07-24 23:57:59 -0500

    update accordion to use 'require' property but need to update controller, which i will do next commit

--- 02_CreatingReusableCompoentsWDirectives/js/directives.js
+++ 02_CreatingReusableCompoentsWDirectives/js/directives.js
@@ -25,7 +25,34 @@
 // });
 
 // Directive via Link
-parking.directive("accordionItem", function () {
+// parking.directive("accordionItem", function () {
+//     return {
+//         templateUrl: "accordionItem.html",
+//         restrict: "E",
+//         scope: {
+//             title: "@"
+//         },
+//         transclude: true,
+//         link: function (scope, element, attrs, ctrl, transcludeFn) {
+//             element.bind("click", function () {
+//                 scope.$apply(function () {
+//                     scope.active = !scope.active;
+//                 });
+//             });
+//         }
+//     };
+// });
+
+// Directive via Require
+parking.directive("accordion", function () {
+    return {
+        template: "<div ng-transclude></div>",
+    restrict: "E",
+    transclude: true
+    };
+});
+
+parking.directive('accordionItem', function () {
     return {
         templateUrl: "accordionItem.html",
         restrict: "E",
@@ -33,6 +60,7 @@ parking.directive("accordionItem", function () {
             title: "@"
         },
         transclude: true,
+        require: "^accordion",
         link: function (scope, element, attrs, ctrl, transcludeFn) {
             element.bind("click", function () {
                 scope.$apply(function () {
