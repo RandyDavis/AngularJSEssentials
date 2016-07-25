03445411dd3785804b2f26b6d49a470500e30bb1
Randy Davis <randy_davis@outlook.com>
2016-07-25 00:08:36 -0500

    update accordion controller when using 'require' and 'link' properties

--- 02_CreatingReusableCompoentsWDirectives/js/directives.js
+++ 02_CreatingReusableCompoentsWDirectives/js/directives.js
@@ -43,12 +43,58 @@
 //     };
 // });
 
-// Directive via Require
+// // Directive via Require
+// parking.directive("accordion", function () {
+//     return {
+//         template: "<div ng-transclude></div>",
+//     restrict: "E",
+//     transclude: true
+//     };
+// });
+//
+// parking.directive('accordionItem', function () {
+//     return {
+//         templateUrl: "accordionItem.html",
+//         restrict: "E",
+//         scope: {
+//             title: "@"
+//         },
+//         transclude: true,
+//         require: "^accordion",
+//         link: function (scope, element, attrs, ctrl, transcludeFn) {
+//             element.bind("click", function () {
+//                 scope.$apply(function () {
+//                     scope.active = !scope.active;
+//                 });
+//             });
+//         }
+//     };
+// });
+
+// Directive via Link - w/updated controller
 parking.directive("accordion", function () {
     return {
         template: "<div ng-transclude></div>",
-    restrict: "E",
-    transclude: true
+        restrict: "E",
+        transclude: true,
+        controller: function ($scope, $element, $attrs, $transclude) {
+            var accordionItems = [];
+
+            var addAccordionItem = function (accordionScope) {
+                accordionItems.push(accordionScope);
+            };
+
+            var closeAll = function () {
+                angular.forEach(accordionItems, function (accordionScope) {
+                    accordionScope.active = false;
+                });
+            };
+
+            return {
+                addAccordionItem: addAccordionItem,
+                closeAll: closeAll
+            };
+        }
     };
 });
 
@@ -63,6 +109,7 @@ parking.directive('accordionItem', function () {
         require: "^accordion",
         link: function (scope, element, attrs, ctrl, transcludeFn) {
             element.bind("click", function () {
+                ctrl.closeAll();
                 scope.$apply(function () {
                     scope.active = !scope.active;
                 });
