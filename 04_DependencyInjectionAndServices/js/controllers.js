e1b582aa43e65c41b822a17b85a0d386330eeeb8
Randy Davis <randy_davis@outlook.com>
2016-07-27 23:34:30 -0500

    create 04_DependencyInjectionAndServices directory containing the previous section's code for use in this lesson

---
+++ 04_DependencyInjectionAndServices/js/controllers.js
@@ -0,0 +1,20 @@
+/**
+ * Created by randy on 7/24/16.
+ */
+parking.controller("parkingCtrl", ["$scope","$filter", function ($scope, $filter) {
+    $scope.appTitle = $filter("uppercase")("[Packt] Parking");
+    $scope.showAlert = true;
+    $scope.alertTopic = "Something went wrong!";
+    $scope.alertDescription = "You must inform the plate and the color of the car!";
+    $scope.closeAlert = function () {
+        $scope.showAlert = false;
+    }
+    $scope.cars = [];
+    $scope.colors = ["White", "Black", "Blue", "Red", "Silver"];
+    $scope.park = function(car) {
+        car.entrance = new Date();
+        $scope.cars.push(car);
+        delete $scope.car;
+    }
+
+}])
