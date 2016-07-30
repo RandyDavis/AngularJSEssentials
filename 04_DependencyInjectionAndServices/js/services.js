b885ddb53a3e7ee6dcf34e6fafd8f51b5c03a66b
Randy Davis <randy_davis@outlook.com>
2016-07-30 00:21:41 -0500

    complete "Creating Service with the service" lesson;

--- 04_DependencyInjectionAndServices/js/services.js
+++ 04_DependencyInjectionAndServices/js/services.js
@@ -1,19 +1,34 @@
 /**
  * Created by randy on 7/28/16.
  */
-parking.factory("parkingService", function (parkingConfig) {
-    var _calculateTicket = function (car) {
+// factory way
+// parking.factory("parkingService", function (parkingConfig) {
+//     var _calculateTicket = function (car) {
+//         var departHour = new Date().getHours();
+//         var entranceHour = car.entrance.getHours();
+//         var parkingPeriod = departHour = entranceHour;
+//         var parkingPrice = parkingPeriod * parkingConfig.parkingRate;
+//         return {
+//             period: parkingPeriod,
+//             price: parkingPrice
+//         };
+//     };
+//
+//     return {
+//         calculateTicket: _calculateTicket
+//     };
+// });
+
+// service way
+parking.service("parkingService", function (parkingConfig) {
+    this.calculateTicket = function (car) {
         var departHour = new Date().getHours();
         var entranceHour = car.entrance.getHours();
-        var parkingPeriod = departHour = entranceHour;
+        var parkingPeriod = departHour - entranceHour;
         var parkingPrice = parkingPeriod * parkingConfig.parkingRate;
         return {
             period: parkingPeriod,
             price: parkingPrice
         };
-    };
-
-    return {
-        calculateTicket: _calculateTicket
     };
 });
