fa5044e93cbcc97feed4db8309e8b1cf9461fb15
Randy Davis <randy_davis@outlook.com>
2016-07-30 00:28:51 -0500

    complete "Creating Service with the provider" lesson;

---
+++ 04_DependencyInjectionAndServices/js/config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by randy on 7/30/16.
+ */
+parking.config(function (parkingServiceProvider) {
+    parkingServiceProvider.setParkingRate(10);
+});
--- 04_DependencyInjectionAndServices/js/services.js
+++ 04_DependencyInjectionAndServices/js/services.js
@@ -19,16 +19,42 @@
 //     };
 // });
 
+
 // service way
-parking.service("parkingService", function (parkingConfig) {
-    this.calculateTicket = function (car) {
+// parking.service("parkingService", function (parkingConfig) {
+//     this.calculateTicket = function (car) {
+//         var departHour = new Date().getHours();
+//         var entranceHour = car.entrance.getHours();
+//         var parkingPeriod = departHour - entranceHour;
+//         var parkingPrice = parkingPeriod * parkingConfig.parkingRate;
+//         return {
+//             period: parkingPeriod,
+//             price: parkingPrice
+//         };
+//     };
+// });
+
+
+// provider way
+parking.provider("parkingService", function (parkingConfig) {
+    var _parkingRate = parkingConfig.parkingRate;
+
+    var _calculateTicket = function (car) {
         var departHour = new Date().getHours();
         var entranceHour = car.entrance.getHours();
         var parkingPeriod = departHour - entranceHour;
-        var parkingPrice = parkingPeriod * parkingConfig.parkingRate;
+        var parkingPrice = parkingPeriod * _parkingRate;
         return {
             period: parkingPeriod,
             price: parkingPrice
         };
     };
+    this.setParkingRate = function (rate) {
+        _parkingRate = rate;
+    };
+    this.$get(function () {
+        return {
+            calculateTicket: _calculateTicket
+        };
+    });
 });
